97196dfb496bde0db3be5ea606068957046fc3fc
Hrithik <hrithikroy1854@gmail.com>
2024-07-28 01:00:07 +0530

    backend build done

--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.js";
+
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
+
 const app = express();
-const PORT = 5000;
-app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.json({ success: true, data: products });
-  } catch (error) {
-    console.error("Error in Get products:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.post("/api/products", async (req, res) => {
-  const product = req.body; // user will send this data
+app.use("/api/products", productRoutes);
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error("Error in Create product:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.put("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = req.body;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res
-      .status(404)
-      .json({ success: false, message: "Product Not Found" });
-  }
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-      new: true,
-    });
-    res.status(200).json({ success: true, data: updatedProduct });
-  } catch (error) {
-    console.error("Error in Update product:", error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product Deleted" });
-  } catch (error) {
-    console.error("Error in Delete product:", error.message);
-    res.status(500).json({ success: false, message: "Product Not Found" });
-  }
-});
-console.log(process.env.MONGO_URI);
 app.listen(PORT, () => {
   connectDB();
-  console.log(`Listening on port http://localhost:${PORT}`);
+  console.log("Server started at http://localhost:" + PORT);
 });
