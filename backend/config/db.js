f328699bb9d89beb99a5b2e1b3a19f925c3bf481
Hrithik <hrithikroy1854@gmail.com>
2024-07-28 00:04:53 +0530

    basic api build done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`error: ${error.message}`);
-    process.exit(1);
+    console.error(`Error: ${error.message}`);
+    process.exit(1); // process code 1 code means exit with failure, 0 means success
   }
 };
